021b59ee5527cc4a3e71f2982867f0c8f0d32a55
Austin <49850194+datkat21@users.noreply.github.com>
2020-08-13 15:01:51 -0500

    Add new powershell script :O

---
+++ citrusos/setup.ps1
@@ -0,0 +1,13 @@
+Remove-Item 'C:\cstemp' -Recurse
+Remove-Item 'C:\CitrusOS' -Recurse
+MkDir C:\cstemp
+MkDir C:\CitrusOS
+ChDir C:\cstemp
+wget https://pieos.xyz/pkmrep/test/test.ppsx -o test.ppsx
+wget https://pieos.xyz/citrusos/7z.exe -o 7z.exe
+wget https://pieos.xyz/citrusos/setup.zip -o setup.zip
+Move-Item C:\cstemp\setup.zip C:\CitrusOS\
+ChDir C:\CitrusOS\
+./7z.exe x setup.zip
+Remove-Item setup.zip
+./CitrusOS.ppsx
