a1ae296b0472283a05ceed8bb0eae828bcd2fd63
datkat21 <49850194+datkat21@users.noreply.github.com>
2020-08-14 08:01:50 -0500

    Fix script again, remove "files" folder

--- citrusos/setup.ps1
+++ citrusos/setup.ps1
@@ -6,6 +6,5 @@ wget https://pieos.xyz/citrusos/7z.exe -o 7z.exe
 ./7z.exe x setup.zip
 Remove-Item setup.zip
 Remove-Item 7z.exe
-cd files
 .\CitrusOS.ppsx
 Remove-Item -LiteralPath C:\CitrusOS\setup.ps1 -Force
